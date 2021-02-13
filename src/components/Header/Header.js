a1c665b98c7653f10d3a5e908c6e3b8d0d41a185
Max <example@gmail.com>
2021-02-13 13:29:07 +0200

    fix color menu on mobile devices

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NavMenu from "../NavMenu/NavMenu";
 import MinimizeNavMenu from "../MinimizeNavMenu/MinimizeNavMenu";
+import CartIcon from "../../assets/images/cart.svg";
 import { NavLink } from "react-router-dom";
 import { withRouter } from "react-router";
 
 import styles from "./Header.module.css";
 
 const Header = ({ cartItemsCount, match }) => {
-  console.log(match);
+  const isMobile = document.documentElement.clientWidth < 500;
 
   return (
     <header className={`${match.isExact ? styles.headerMain : ""}`}>
@@ -16,7 +17,10 @@ const Header = ({ cartItemsCount, match }) => {
       </span>
       <MinimizeNavMenu />
       <span className={styles.headerCart}>
-        <NavLink to="/cart">{`Корзина [${cartItemsCount}]`}</NavLink>
+        <NavLink to="/cart">
+          {isMobile ? `[${cartItemsCount}]` : `Корзина [${cartItemsCount}]`}
+          {isMobile ? <img src={CartIcon} alt="Иконка корзины" /> : ""}
+        </NavLink>
       </span>
       <NavMenu />
     </header>
